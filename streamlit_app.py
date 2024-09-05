3949ebefa0b1044370b7da0458f9ec8b6234cf8d
Enlighten <wsjsxzzgq@163.com>
2024-09-05 16:48:46 +0800

    streamlit test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
+import openai
 import streamlit as st
 
-name = st.text_input('请输入你的名字')
-age = st.slider('请选择你的年龄', 0, 100, 25)
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-st.write(f'你好，{name}！你的年龄是{age}岁。')
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    # 申明openai_key
+    openai.api_key = openai_api_key
+    # 将user的输入添加到session里面
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # 将user的输入展示到页面的对话框中
+    st.chat_message("user").write(prompt)
+    # 调用openai的接口，获取chatgpt的回复
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    # 将openai的回复添加到session里面
+    st.session_state.messages.append(msg)
+    # 将openai的回复展示到对话框里面
+    st.chat_message("assistant").write(msg.content)
